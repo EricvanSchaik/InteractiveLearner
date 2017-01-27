9ce8b761de7d7f9e83db7a78a665beecaa902513
Eric <schaikejgv@gmail.com>
2017-01-27 22:49:50 +0100

    Done

--- src/com/company/Train.java
+++ src/com/company/Train.java
@@ -15,6 +15,7 @@ public class Train {
 	public static int amountNormal;
 	public static int correct = 0;
 	public static int incorrect = 0;
+	public static List<String> vocabulary = new ArrayList<>();
 
 	public static void main(String[] args) {
 		try {
@@ -37,18 +38,18 @@ public class Train {
 				}
 			}
 			System.out.println("Training done");
-			System.out.println("Begin testing...");
-
-			File normalTestDir = new File("C:\\Users\\Eric\\IdeaProjects\\InteractiveLearner\\src\\corpus-mails\\corpus\\test\\normal");
-			File[] normalTestFiles = normalTestDir.listFiles();
-			File spamTestDir = new File("C:\\Users\\Eric\\IdeaProjects\\InteractiveLearner\\src\\corpus-mails\\corpus\\test\\spam");
-			File[] spamTestFiles = spamTestDir.listFiles();
-			testFiles(normalTestFiles, false);
-			testFiles(spamTestFiles, true);
-			System.out.println("Correct: " + correct + "\nIncorrect: " + incorrect);
-			double totalClassified = correct+incorrect;
-			double accuracy = ((double)correct)/totalClassified;
-			System.out.println("Accuracy: " + accuracy);
+//			System.out.println("Begin testing...");
+//
+//			File normalTestDir = new File("C:\\Users\\Eric\\IdeaProjects\\InteractiveLearner\\src\\corpus-mails\\corpus\\test\\normal");
+//			File[] normalTestFiles = normalTestDir.listFiles();
+//			File spamTestDir = new File("C:\\Users\\Eric\\IdeaProjects\\InteractiveLearner\\src\\corpus-mails\\corpus\\test\\spam");
+//			File[] spamTestFiles = spamTestDir.listFiles();
+//			testFiles(normalTestFiles, false);
+//			testFiles(spamTestFiles, true);
+//			double accuracy = ((double)correct)/((double)correct+(double)incorrect);
+//			System.out.println("Accuracy: " + accuracy);
+//			System.out.println("Vocabulary size: " + vocabulary.size());
+//			System.out.println("Baseline: " + ((double)normalTestFiles.length)/(double)(normalTestFiles.length+spamTestFiles.length));
 		} catch (IOException | NullPointerException e) {
 			e.printStackTrace();
 		}
@@ -81,6 +82,7 @@ public class Train {
 				for (String word : words) {
 					if (!countlst.containsKey(word)) {
 						countlst.put(word, 1);
+						vocabulary.add(word);
 						exists.add(word);
 					} else if (!exists.contains(word)) {
 						int initValue = countlst.get(word).intValue();
@@ -106,9 +108,9 @@ public class Train {
 		//Delete all stopwords
 		List<String> stopwords = Arrays.asList("subject:", "a", "above", "about", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "intro", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over","own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what",  "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll",  "you're", "you've", "your", "yours", "yourself", "yourselves");
 		List<String> words = new LinkedList<>(Arrays.asList(result.split(" ")));
-		System.out.println("BEFORE DELETING STOPWORDS: " + words.toString());
+//		System.out.println("BEFORE DELETING STOPWORDS: " + words.toString());
 		words.removeAll(stopwords);
-		System.out.println("AFTER DELETING STOPWORDS: " + words.toString());
+//		System.out.println("AFTER DELETING STOPWORDS: " + words.toString());
 
 		// Delete all special characters
 		int a = Character.getNumericValue("a".charAt(0));
@@ -119,7 +121,7 @@ public class Train {
                 iterator.remove();
             }
         }
-        System.out.println("AFTER DELETING SPECIAL SYMBOLS: " + words.toString());
+//        System.out.println("AFTER DELETING SPECIAL SYMBOLS: " + words.toString());
 		return words.toArray(new String[words.size()]);
 	}
 
